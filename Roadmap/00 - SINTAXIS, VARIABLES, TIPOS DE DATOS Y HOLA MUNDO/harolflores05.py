e837689ca8f3421b9b7eea6c7694617ac8be819a
harolflores05 <167864553+harolflores05@users.noreply.github.com>
2024-04-29 15:19:56 +0200

    Create harolflores05.py
    harolflores05.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/harolflores05.py
@@ -0,0 +1,35 @@
+#WEB OFICIAL PYTHON
+#https://www.learnpython.org/es/
+
+#Comentario en una linea
+
+"""
+Esto es un 
+comentario en 
+varias lineas
+"""
+
+'''
+Esto es un 
+comentario en 
+varias lineas
+'''
+my_variable = 'Mi variable'
+my_variable = 'Nuevo valor de mi variable'
+
+MY_CONSTANT = 'Mi constante'  # por convención
+
+
+my_int =  1 
+my_float = 1.5
+my_bool = True
+my_bool =  False
+my_string = 'Mi cadena de texto'
+my_other_string = 'Mi otra cadena de texto'
+
+print('Hola, Python')
+print('my_int')
+print('my_float')
+print("my_bool")
+print('my_string')
+print('my_other_string')
